94d812323d8364acb2783bceb42b7719ae88f980
mignonnesaurus <elifloresch@gmail.com>
2017-12-03 16:38:26 +0100

    remove utils

--- test/item.js
+++ test/item.js
@@ -1,14 +1,8 @@
 import test from 'ava'
 import request from 'supertest'
-
-import { before, beforeEach, afterEach, after } from './utils'
-
-test.before(before)
-test.beforeEach(beforeEach)
-test.afterEach.always(afterEach)
+import app from '../src/app'
 
 test('Get list of items', async t => {
-    const { app } = t.context
     const itemToCreate = { name: 'Flour', quantity: 2 }
 
    await request(app)
@@ -21,10 +15,9 @@ test('Get list of items', async t => {
     t.is(res.status, 200)
     t.true(Array.isArray(res.body))
     t.true(res.body.length > 0)
-})
+});
 
 test('Create new item', async t => {
-    const { app } = t.context
     const itemToCreate = { name: 'Flour', quantity: 2 }
 
     const res = await request(app)
@@ -34,11 +27,9 @@ test('Create new item', async t => {
     t.is(res.status, 200)
     t.is(res.body.name, itemToCreate.name)
     t.is(res.body.quantity, itemToCreate.quantity)
-})
+});
 
 test('Fetch an item', async t => {
-    const { app } = t.context
-
     const item = (await request(app)
         .post('/item')
         .send({ name: 'Flour', quantity: 2 }))
@@ -49,11 +40,9 @@ test('Fetch an item', async t => {
 
     t.is(fetch.status, 200)
     t.deepEqual(fetch.body, item)
-})
+});
 
 test('Fetch items queried name', async t => {
-    const { app } = t.context
-
     const item = (await request(app)
         .post('/item')
         .send({ name: 'Salt', quantity: 2 }))
@@ -66,15 +55,13 @@ test('Fetch items queried name', async t => {
     t.true(Array.isArray(fetch.body))
     t.is(fetch.body.length, 1)
     t.deepEqual(fetch.body[0], item)
-})
+});
 
 test('Fetch an items queried name when item does not exist', async t => {
-    const { app } = t.context
-
-    const item = (await request(app)
+    (await request(app)
         .post('/item')
         .send({ name: 'Butter', quantity: 2 }))
-        .body
+        .body;
 
     const fetch = await request(app)
         .get(`/item?name=chocolate`)
@@ -82,11 +69,9 @@ test('Fetch an items queried name when item does not exist', async t => {
     t.is(fetch.status, 200)
     t.true(Array.isArray(fetch.body))
     t.is(fetch.body.length, 0)
-})
+});
 
 test('Update quantity', async t => {
-    const { app } = t.context
-
     const item = (await request(app)
         .post('/item')
         .send({ name: 'Peper', quantity: 2 }))
@@ -102,12 +87,10 @@ test('Update quantity', async t => {
 
     t.is(fetch.status, 200)
     t.deepEqual(fetch.body, updatedItem)
-})
+});
 
 test('Delete an item', async t => {
     t.plan(3)
-
-    const { app } = t.context
 
     const item = (await request(app)
         .post('/item')
@@ -124,6 +107,4 @@ test('Delete an item', async t => {
         .get(`/item/${item.id}/json`)
 
     t.is(fetch.status, 404)
-})
-
-test.after.always(after)
+});
